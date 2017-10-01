ff16f2bffa5ffa5d8e2f7fd28cfa700ad4a6ca93
alexeyemel <alexey022@mail.ru>
2017-10-02 00:00:05 +0300

    Update SimpleJavaClass.java

--- jarModule/src/main/java/com/pvt/SimpleJavaClass.java
+++ jarModule/src/main/java/com/pvt/SimpleJavaClass.java
@@ -1,6 +1,6 @@
 package com.pvt;
 
 public class SimpleJavaClass {
-    public static double getSquare(int r){return Math.PI*r*r;}
-    public static double getLength(int r){return 2*Math.PI*r;}
+    public static double getSquare(int r){return Math.PI*r*r*145;}
+    public static double getLength(int r){return 2*Math.PI*r*34;}
 }
