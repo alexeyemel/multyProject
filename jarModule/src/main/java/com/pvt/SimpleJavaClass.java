5ac94c1475cfe5fe64e03cd7ca6e415e64766bbb
Alexey Yemelyanov <alexey022@mail.ru>
2017-10-02 00:14:28 +0300

    После изменения amend Новое название комита

--- jarModule/src/main/java/com/pvt/SimpleJavaClass.java
+++ jarModule/src/main/java/com/pvt/SimpleJavaClass.java
@@ -1,6 +1,6 @@
 package com.pvt;
 
 public class SimpleJavaClass {
-    public static double getSquare(int r){return Math.PI*5;}
-    public static double getLength(int r){return 2*4;}
+    public static double getSquare(int r){return Math.PI*r*r;}
+    public static double getLength(int r){return 2*Math.PI*r;}
 }
