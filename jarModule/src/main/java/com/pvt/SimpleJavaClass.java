ed705ed752d94d0091a1999715f72dcc4108634a
Alexey Yemelyanov <alexey022@mail.ru>
2017-10-01 23:36:47 +0300

    commit 1

---
+++ jarModule/src/main/java/com/pvt/SimpleJavaClass.java
@@ -0,0 +1,6 @@
+package com.pvt;
+
+public class SimpleJavaClass {
+    public static double getSquare(int a,int b){return a*b;}
+    public static double getPerimeter(int a,int b){return 2*(a+b);}
+}
